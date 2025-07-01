c0cb07911700b0cbba28be73195eac42af9089ae
johnnyo21 <john@cgfx.co>
2025-07-01 13:24:53 -0600

    Update floorplan-3d-card.js

--- floorplan-3d-card.js
+++ floorplan-3d-card.js
@@ -394,6 +394,7 @@ class Floorplan3dCard extends LitElement {
                 if (state.state === 'on') {
                     const brightness = state.attributes.brightness !== undefined ? (state.attributes.brightness / 255) : 1;
                     lightData.targetIntensity = lightData.maxIntensity * brightness;
+                    console.log(`Floorplan3D-Card: Setting light ${entity_id} to intensity ${lightData.targetIntensity}`);
                 } else {
                     lightData.targetIntensity = 0;
                 }
